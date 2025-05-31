f2eb18b3c248599a20688a9b9272ca6a5ff33e56
Vajran-K <kvajran@gmail.com>
2025-05-31 18:55:06 +0530

    Updated

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ let myTab = []
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteEl = document.getElementById("delete-btn")
 
-inputBtn.addEventListener("click", function(){
-    myTab.push(inputEl.value)
-    inputEl.value = ""
-    renderTabs()
-})
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myTab") )
 
-function renderTabs(){
+if(leadsFromLocalStorage){
+    myTab = leadsFromLocalStorage
+    renderTabs(myTab)
+}
+
+function renderTabs(Tabs){
     let listItems = ""
 
-    for(let i = 0 ; i < myTab.length; i++){
+    for(let i = 0 ; i < Tabs.length; i++){
         listItems += `
             <li>
-                <a target='_blank' href='${myTab[i]}'>
-                    ${myTab[i]}
+                ->
+                <a target='_blank' href='${Tabs[i]}'>
+                    ${Tabs[i]}
                 </a>
             </li>
         `
@@ -25,4 +29,27 @@ function renderTabs(){
 
     ulEl.innerHTML = listItems
 }
+
+deleteEl.addEventListener("dblclick", function() {
+    localStorage.removeItem("myTab")
+    myTab = []
+    renderTabs(myTab)
+})
+
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+        myTab.push(tabs[0].url)
+        localStorage.setItem("myTab",JSON.stringify(myTab))
+        renderTabs(myTab)
+    })
+})
+
+inputBtn.addEventListener("click", function(){
+    myTab.push(inputEl.value)
+    localStorage.setItem("myTab",JSON.stringify(myTab))
+    inputEl.value = ""
+    renderTabs(myTab)
+
+})
+
 
